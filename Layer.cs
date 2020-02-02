be310f546dfaa1f19c4f9814a03ba997d008c62c
Your Name <you@example.com>
2020-02-02 11:50:10 +0200

    Initial commit

---
+++ Layer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeuralNetwork
+{
+    public class Layer
+    {
+        public enum Activation {Sigmoid, Relu}
+        private float[] _neurons;
+        private float[,] _weights;
+        private Activation _activation;
+        public Layer(int neurons, int next_neurons, Activation activation = Activation.Sigmoid)
+        {
+            _neurons = new float[neurons];
+            _weights = new float[neurons, next_neurons];
+            _activation = activation;
+        }
+
+        public void IniatilizeWeights()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < _weights.GetLength(0); i++)
+            {
+                for (int j = 0; j < _weights.GetLength(1); j++)
+                {
+                    _weights[i, j] = (float)(rand.NextDouble()) * 4 - 2;
+                }
+            }
+        }
+    }
+}
---
+++ NeuralNetwork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        private List<Layer> layers = new List<Layer>();
+        private List<Tuple<int, Layer.Activation>> layerData = new List<Tuple<int, Layer.Activation>>();
+        public void AddLayer(int neurons, Layer.Activation activation = Layer.Activation.Sigmoid)
+        {
+
+        }
+        public void Compile()
+        {
+
+        }
+        static void Main()
+        {
+            Layer layer = new Layer(3, 5);
+            layer.IniatilizeWeights();
+            Console.ReadKey();
+        }
+    }
+}
